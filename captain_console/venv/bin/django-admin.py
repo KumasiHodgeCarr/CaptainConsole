71ffcbc538e6d0c96e007e030d479beac9113bfb
kumasi <kumasi19@ru.is>
2020-05-02 19:11:14 +0000

    búið til flest öpp

---
+++ captain_console/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kumasi/Desktop/django/CaptainConsole/captain_console/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
