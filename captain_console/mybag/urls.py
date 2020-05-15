d5a672990490b1d899d5b22ba6528498741150aa
robbiingi <robert19@ru.is>
2020-05-15 22:57:11 +0000

    Lagfæra nokkra hluti fyrir skil, ekki full klárað

--- captain_console/mybag/urls.py
+++ captain_console/mybag/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name ='mybagindex')
+    path('', views.index, name ='mybagindex'),
+    path('checkout.html', views.checkout, name='checkout')
 ]
--- captain_console/mybag/views.py
+++ captain_console/mybag/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'mybag/index.html')
+
+def checkout(request):
+    return render(request, 'mybag/checkout.html')
